082650aca3d3eb5a33a98f0c35f06ec2c6c56037
Kushal Kumar Banyal <kushal1748.be21@chitkara.edu.in>
2024-12-11 23:49:00 +0530

    search done but error in production not resolved

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,8 @@ import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
-const Home = async ({ searchParams }: SearchParamProps) => {
+
+const Home = async ({searchParams}: SearchParamProps) => {
 
   const search = await searchParams;
   const page = Number(search?.page) || 1;
