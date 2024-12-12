267228b5145cc4013c6b55fea1a462b3ca4af6d1
Kushal Kumar Banyal <kushal1748.be21@chitkara.edu.in>
2024-12-12 22:04:32 +0530

    profile page params handled

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -8,11 +8,10 @@ import { getUserImages } from "@/lib/actions/image.actions";
 import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs/server";
 
-const Profile = async ({searchParams}:SearchParamProps) => {
+const Profile = async ({searchParams}:{searchParams:Promise<any>}) => {
   const search = await searchParams;
   const page =  Number( search?.page) || 1;
   const { userId } = await auth();
-
   if (!userId) redirect("/sign-in");
 
   const user = await getUserById(userId);
